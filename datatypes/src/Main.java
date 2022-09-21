1c3d44328e87ec8fe276a9e235c46a4b9df4dbfa
yozmen <yozmen@gmail.com>
2022-09-21 20:46:27 +0300

    commit

---
+++ datatypes/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int sayi = 12;
+        sayi = 127;
+
+        double ondalikliSayi = 12.8;
+
+        char karakter = 'B';
+
+        boolean dogruMu = true;
+
+        System.out.println("int = " + sayi);
+        System.out.println("double = " + ondalikliSayi);
+        System.out.println("char = " + karakter);
+        System.out.println("boolean = " + dogruMu);
+
+    }
+}
